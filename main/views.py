7eb12e7e2c584902c81324b9d316625eb7f02468
benso <fdavisarz10@gmail.com>
2025-01-05 21:47:32 +0300

    fron-end

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Products, Category
 
 def popular_list(request):
-    products = Products.objects.filter(available=True)[:3]
+    products = Products.objects.filter(available=True)[:5]
     return render(request, 'main/index/index.html',
                   {"products":products})
 
